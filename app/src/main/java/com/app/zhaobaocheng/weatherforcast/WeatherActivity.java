b0a4dfd534308c152cc9e690baa896574ddf25b0
zhaobaocheng <changeszbc@gmail.com>
2017-03-22 23:20:25 +0800

    天气显示界面

---
+++ app/src/main/java/com/app/zhaobaocheng/weatherforcast/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.app.zhaobaocheng.weatherforcast;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZhaoBaocheng on 2017/3/22.
+ */
+public class WeatherActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
